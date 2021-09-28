f997c50630e01f91466125fae9b4601bf03b3e56
abduvakhidov-kh <habibulloabduvahidov03@gmail.com>
2021-09-28 16:47:06 +0500

    banner have done
    banner component have completly done

--- src/components/Navabar/Navbar.jsx
+++ src/components/Navabar/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.css";
 
 export default function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 130) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="navbar">
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt="netflix logo"
